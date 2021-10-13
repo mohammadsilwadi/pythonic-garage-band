e37ab2d1f13484bee80f009a9664ab3bee7f4733
mohammad silwadi <mohammadsilwadi1@gmail.com>
2021-10-13 21:19:12 +0300

    test:test 6 pass

--- tests/test_band.py
+++ tests/test_band.py
@@ -41,7 +41,7 @@ def test_drummer_repr():
     assert actual == expected
 
 
-
+# @pytest.mark.skip("todo")
 def test_bassist_str():
     meshell = Bassist("Meshell Ndegeocello")
     actual = str(meshell)
@@ -49,7 +49,7 @@ def test_bassist_str():
     assert actual == expected
 
 
-@pytest.mark.skip("todo")
+# @pytest.mark.skip("todo")
 def test_bassist_repr():
     meshell = Bassist("Meshell Ndegeocello")
     actual = repr(meshell)
